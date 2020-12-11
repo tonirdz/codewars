fba865901ae76262b645ef3e510634a92cd70fc8
Toni Rodríguez Sáez <33283061+tonirdz@users.noreply.github.com>
2020-12-12 00:20:26 +0100

    feat(8 kyu): Will there be enough space?

---
+++ src/test/java/_8kyu/Will_There_Be_Enough_SpaceTest.java
@@ -0,0 +1,15 @@
+package _8kyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Will_There_Be_Enough_SpaceTest {
+
+    @Test
+    void enough() {
+        assertEquals(0, Will_There_Be_Enough_Space.enough(10, 5, 5));
+        assertEquals(10, Will_There_Be_Enough_Space.enough(100, 60, 50));
+        assertEquals(0, Will_There_Be_Enough_Space.enough(20, 5, 5));
+    }
+}
