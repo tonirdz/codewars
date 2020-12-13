e0c4a4ab8321e5613ec94a5c32f53a99512aca9b
Toni Rodríguez Sáez <33283061+tonirdz@users.noreply.github.com>
2020-12-14 00:56:44 +0100

    feat(8 kyu): Sum Arrays

---
+++ src/main/java/_8kyu/Sum_Arrays.java
@@ -0,0 +1,9 @@
+package _8kyu;
+
+import java.util.stream.DoubleStream;
+
+public class Sum_Arrays {
+    public static double sum(double[] numbers) {
+        return DoubleStream.of(numbers).sum();
+    }
+}
