6be4f024612d166abd3cc7a6dfdf843abfce9ffd
Toni Rodríguez Sáez <33283061+tonirdz@users.noreply.github.com>
2020-12-14 00:56:21 +0100

    feat(8 kyu): Switch it Up!

---
+++ src/main/java/_8kyu/Switch_It_Up.java
@@ -0,0 +1,20 @@
+package _8kyu;
+
+public class Switch_It_Up {
+    public static String switchItUp(int number)
+    {
+        return switch (number) {
+            case 0 -> "Zero";
+            case 1 -> "One";
+            case 2 -> "Two";
+            case 3 -> "Three";
+            case 4 -> "Four";
+            case 5 -> "Five";
+            case 6 -> "Six";
+            case 7 -> "Seven";
+            case 8 -> "Eight";
+            case 9 -> "Nine";
+            default -> "";
+        };
+    }
+}
