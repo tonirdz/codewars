035890d880e428c887b4a808c73c5d5da22342be
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2020-12-21 00:24:37 +0100

    feat(6 kyu): Stop gninnipS My sdroW

---
+++ src/test/java/_6kyu/SpinWordsTest.java
@@ -0,0 +1,14 @@
+package _6kyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpinWordsTest {
+
+    @Test
+    public void test() {
+        assertEquals("emocleW", new SpinWords().spinWords("Welcome"));
+        assertEquals("Hey wollef sroirraw", new SpinWords().spinWords("Hey fellow warriors"));
+    }
+}
