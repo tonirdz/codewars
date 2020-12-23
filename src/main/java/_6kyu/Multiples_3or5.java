3fa135309f020b08f4854d57c62ee1402e12d609
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2020-12-24 00:52:49 +0100

    feat(6 kyu): Multiples of 3 or 5

---
+++ src/main/java/_6kyu/Multiples_3or5.java
@@ -0,0 +1,9 @@
+package _6kyu;
+
+import java.util.stream.IntStream;
+
+public class Multiples_3or5 {
+    public int solution(int number) {
+        return IntStream.range(0,number).filter(i -> (i % 3 == 0) || (i % 5 == 0)).sum();
+    }
+}
