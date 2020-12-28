9094b798d802459c1b187598458f83c7f95cea42
Toni Rodríguez Sáez <33283061+tonirdz@users.noreply.github.com>
2020-12-28 23:41:10 +0100

    feat(8 kyu): Get the mean of an array

---
+++ src/main/java/_8kyu/Mean_Of_Array.java
@@ -0,0 +1,9 @@
+package _8kyu;
+
+import java.util.Arrays;
+
+public class Mean_Of_Array {
+    public static int getAverage(int[] marks){
+        return (int) Arrays.stream(marks).summaryStatistics().getAverage();
+    }
+}
