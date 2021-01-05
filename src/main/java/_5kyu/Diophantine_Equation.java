f7ab87090d71600f29c41abf0f90b42e1a506c8f
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2021-01-06 00:59:09 +0100

    feat(8 kyu): Reversed Words

--- src/main/java/_5kyu/Diophantine_Equation.java
+++ src/main/java/_5kyu/Diophantine_Equation.java
@@ -1,19 +1,19 @@
 package _5kyu;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Diophantine_Equation {
+public class Diophantine_Equation extends Thread {
     public static String solEquaStr(long n) {
-        ArrayList<ArrayList<Long>> xy = new ArrayList<>();
+        StringBuilder result = new StringBuilder("[");
         for (long a = n; a >= Math.sqrt(n); a--) {
             if (n % a == 0) {
                 long b = n / a;
-                long x = (a + b) >> 1;
-                long y = (a - b) >> 2;
-                if (((a + b) % 2 == 0) && ((b - a) % 4 == 0)) xy.add(new ArrayList<>(List.of(x, y)));
+                if (((a + b) % 2 == 0) && ((b - a) % 4 == 0)) {
+                    long x = (a + b) >> 1;
+                    long y = (a - b) >> 2;
+                    result.append("[").append(x).append(", ").append(y).append("], ");
+                }
             }
         }
-        return xy.toString();
+        if (result.toString().equals("[")) return "[]";
+        return result.substring(0, result.length() -2) + "]";
     }
 }
