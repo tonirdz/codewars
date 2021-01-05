55d77a030f2425c7c57ec783aff014aac0f9743a
Toni Rodríguez Sáez <33283061+tonirdz@users.noreply.github.com>
2021-01-06 00:59:09 +0100

    feat(8 kyu): Reversed Words

---
+++ src/main/java/_8kyu/Reversed_Words.java
@@ -0,0 +1,13 @@
+package _8kyu;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Reversed_Words {
+    public static String reverseWords(String str) {
+        List<String> words = Arrays.asList(str.split(" "));
+        Collections.reverse(words);
+        return String.join(" ", words);
+    }
+}
