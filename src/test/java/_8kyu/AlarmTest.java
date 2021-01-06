f1a2b3c9cd0eab00b041055cbd0f7dd471c47c84
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2021-01-06 01:09:11 +0100

    feat(8 kyu): L1: Set Alarm

---
+++ src/test/java/_8kyu/AlarmTest.java
@@ -0,0 +1,15 @@
+package _8kyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlarmTest {
+    @Test
+    public void setAlarmTest() {
+        assertTrue(Alarm.setAlarm(true, false),"Should be true.");
+        assertFalse(Alarm.setAlarm(true, true),"Should be false.");
+        assertFalse(Alarm.setAlarm(false, false),"Should be false.");
+        assertFalse(Alarm.setAlarm(false, true), "Should be false.");
+    }
+}
