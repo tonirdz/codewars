7522256808d6e986442b6f9e551a5eaa5787ca74
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2021-01-10 01:12:36 +0100

    feat(7 kyu): Fusion Chamber Shutdown

---
+++ src/test/java/_7kyu/Fusion_Chamber_ShutdownTest.java
@@ -0,0 +1,30 @@
+package _7kyu;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Fusion_Chamber_ShutdownTest {
+
+    @Test
+    public void fixedTests() {
+        SoftAssertions softly = new SoftAssertions();
+        int[][] params = new int[][]{
+                {45, 11, 100, 5, 45, 0},
+                {354, 1023230, 0, 0, 0, 354},
+                {939, 3, 694, 1, 346, 0},
+                {215, 41, 82100, 20, 215, 0},
+                {113, 0, 52, 0, 26, 0}
+        };
+        IntStream.range(0, params.length).forEach(i -> {
+            int[] caseParams = params[i];
+            softly.assertThat(Fusion_Chamber_Shutdown.burner(caseParams[0], caseParams[1], caseParams[2]))
+                    .as(String.format("burner(%s, %s, %s)", caseParams[0], caseParams[1], caseParams[2]))
+                    .containsExactly(caseParams[3], caseParams[4], caseParams[5]);
+        });
+        softly.assertAll();
+    }
+}
