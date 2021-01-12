9e01a3555ff9fc08e9c2e2f627295075c3e5e367
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2021-01-12 23:31:40 +0100

    feat(7 kyu): Vowel Count

---
+++ src/test/java/_7kyu/Vowel_CountTest.java
@@ -0,0 +1,12 @@
+package _7kyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Vowel_CountTest {
+    @Test
+    public void testCase1() {
+        assertEquals(5, Vowel_Count.getCount("abracadabra"), "Nope!");
+    }
+}
