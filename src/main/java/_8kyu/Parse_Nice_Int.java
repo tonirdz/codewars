8b32404597c2100d61613f8330f3b10632d80301
Toni Rodríguez Sáez <33283061+tonirdz@users.noreply.github.com>
2021-01-15 23:45:49 +0100

    feat(8 kyu): Parse nice int from char problem

---
+++ src/main/java/_8kyu/Parse_Nice_Int.java
@@ -0,0 +1,7 @@
+package _8kyu;
+
+public class Parse_Nice_Int {
+    public static int howOld(String herOld) {
+        return Integer.parseInt(herOld.substring(0, 1));
+    }
+}
---
+++ src/test/java/_8kyu/Parse_Nice_IntTest.java
@@ -0,0 +1,23 @@
+package _8kyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Parse_Nice_IntTest {
+
+    @Test
+    public void test1() {
+        assertEquals(5, Parse_Nice_Int.howOld("5 years old"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(9, Parse_Nice_Int.howOld("9 years old"));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(1, Parse_Nice_Int.howOld("1 year old"));
+    }
+}
