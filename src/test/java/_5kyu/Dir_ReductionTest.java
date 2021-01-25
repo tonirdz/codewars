1841c975572f4bcad944811889ffa2d2ee24311a
Toni Rodríguez Sáez <33283061+tonirdz@users.noreply.github.com>
2021-01-25 01:44:34 +0100

    feat(5 kyu): Directions Reduction

---
+++ src/test/java/_5kyu/Dir_ReductionTest.java
@@ -0,0 +1,19 @@
+package _5kyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Dir_ReductionTest {
+    @Test
+    public void testSimpleDirReduc() {
+        assertArrayEquals(
+                new String[]{"WEST"},
+                Dir_Reduction.dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}),
+                "\"NORTH\", \"SOUTH\", \"SOUTH\", \"EAST\", \"WEST\", \"NORTH\", \"WEST\"");
+        assertArrayEquals(
+                new String[]{},
+                Dir_Reduction.dirReduc(new String[]{"NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"}),
+                "\"NORTH\",\"SOUTH\",\"SOUTH\",\"EAST\",\"WEST\",\"NORTH\"");
+    }
+}
