ea4da417e6937ef082d47ae53dddbeb44b46ad65
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2021-01-26 22:56:55 +0100

    feat(7 kyu): Highest and Lowest

---
+++ src/test/java/_7kyu/Highest_LowestTest.java
@@ -0,0 +1,12 @@
+package _7kyu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Highest_LowestTest {
+    @Test
+    public void test1() {
+        assertEquals("42 -9", Highest_Lowest.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    }
+}
