9b06df47f461c68629bf08476e8e1652abdf3677
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2021-03-22 00:36:12 +0100

    feat(6 kyu): Backspaces in string

---
+++ 6-kyu/backspaces-in-string/src/test/java/BackspacesInStringTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BackspacesInStringTest {
+    @Test
+    public void testCleanString() {
+        final BackspacesInString bis = new BackspacesInString();
+        assertEquals("ac", bis.cleanString("abc#d##c"));
+        assertEquals("", bis.cleanString("abc####d##c#"));
+    }
+}
